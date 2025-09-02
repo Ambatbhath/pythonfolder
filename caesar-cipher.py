fe4e3e0daa518b590b1d91e891d8520b20a3e393
Ambatbhath <niveditha.ajan@outlook.com>
2025-09-02 12:38:59 -0500

    Caesar Cipher decoding

--- caesar-cipher.py
+++ caesar-cipher.py
@@ -1,33 +1,42 @@
-def getDoubleAlphabet(alphabet):
-    doubleAlphabet = alphabet + alphabet
-    return doubleAlphabet
 
+#take 3 arguments: the message, the cipher key and the alphabet
+def getDoubleAlphabet(alphabet):
+    doubleAlphabet=alphabet+alphabet
+    return doubleAlphabet
+    
 def getMessage():
-    stringToEncrypt = input("Please enter a message to encrypt: ")
+    stringToEncrypt=input("Please enter a message to encrypt: ")
     return stringToEncrypt
     
 def getCipherKey():
-    shiftAmount = input( "Please enter a key (whole number from 1-25): ")
+    shiftAmount = input("Please enter a key (whole number from 1-25): ")
     return shiftAmount
-    
+
 def encryptMessage(message, cipherKey, alphabet):
-    encryptedMessage = ""
-    uppercaseMessage = ""
-    uppercaseMessage = message.upper()
+    encryptedMessage=""
+    uppercaseMessage=""
+    uppercaseMessage=message.upper()
+    print(f'alphabet is: {alphabet}')
     for currentCharacter in uppercaseMessage:
         position = alphabet.find(currentCharacter)
+        print(f'current character is: {currentCharacter}')
+        print(f'current character position is: {position}')
         newPosition = position + int(cipherKey)
+        print(f'New Encrypted Position of the character is: {newPosition}')
+        print(f'Encrypted character is: {alphabet[newPosition]}')
         if currentCharacter in alphabet:
             encryptedMessage = encryptedMessage + alphabet[newPosition]
+            print(f'Encrypted Message: {encryptedMessage}')
         else:
             encryptedMessage = encryptedMessage + currentCharacter
     return encryptedMessage
     
 def decryptMessage(message, cipherKey, alphabet):
     decryptKey = -1 * int(cipherKey)
-    return encryptMessage(message, decryptKey, alphabet)
+    print(f'Decrypted cipherKey: {decryptKey}')
+    return encryptMessage(message,decryptKey,alphabet)
     
-def runCaesarCipherProgram():
+"""def runCaesarCipherProgram():
     myAlphabet="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     print(f'Alphabet: {myAlphabet}')
     myAlphabet2 = getDoubleAlphabet(myAlphabet)
@@ -38,7 +47,17 @@ def runCaesarCipherProgram():
     print(myCipherKey)
     myEncryptedMessage = encryptMessage(myMessage, myCipherKey, myAlphabet2)
     print(f'Encrypted Message: {myEncryptedMessage}')
-    myDecryptedMessage = decryptMessage(myEncryptedMessage, myCipherKey, myAlphabet2)
-    print(f'Decypted Message: {myDecryptedMessage}')
+    myDecryptedMessage=decryptMessage(myEncryptedMessage,myCipherKey,myAlphabet2)
+    print(f'Decrypted Message: {myDecryptedMessage}')"""
     
-runCaesarCipherProgram()
+#runCaesarCipherProgram()
+message = getMessage()
+print(message)
+doubleAl = getDoubleAlphabet("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+print(doubleAl)
+cipherKey = getCipherKey()
+print(message)
+encryptedMessage=encryptMessage(message,cipherKey,doubleAl)
+print(f'Final Encrypted Message is: {encryptedMessage}')
+decryptedMessage=decryptMessage(encryptedMessage,cipherKey,doubleAl)
+print(f'Final Decrypted Message is: {decryptedMessage}')
